40b861b83a9f2af4a978cd8ae8831bdf09f35c0d
torioLuz <trisianto@gmail.com>
2014-11-16 20:29:33 -0800

    Javascript refined

--- script/myscript.js
+++ script/myscript.js
@@ -3,12 +3,14 @@ var viz, workbook, activeSheet;
 
 $(document).ready(function(){
 
+	var intro = $('#intro');
+	var analysis = $('#analysis');
 	
 	var placeholderDiv = document.getElementById("tableauViz");
 	var url = "http://public.tableausoftware.com/views/ProjectDemandvsSkillCapacity/Dashboard1";
 	var options = {
 		hideTabs: true,
-		width: "800px",   
+		width: "1100px",   
 		height: "700px",
 		onFirstInteractive: function () {
 			workbook = viz.getWorkbook();
@@ -38,10 +40,50 @@ $(document).ready(function(){
 		$(this).addClass('active');
 		filterScenario($(this).text());
 
-
-
-
 	});
+
+	function showSection(show, hide) {
+		$(hide).fadeOut(700).promise()
+		.done(function(){
+			$(show).slideDown(500);	
+
+		});
+
+		if (show === $('#intro')){
+			$('.intro-link').addClass("active").siblings("li").removeClass("active");
+
+
+		}
+		else {
+			$('.analysis-link').addClass("active").siblings("li").removeClass("active");
+	
+		}
+		
+
+	}
+
+
+	$('.button-intro').on('click', function(e){
+		e.preventDefault();
+		showSection(analysis, intro);
+		
+	});
+
+
+	$('.intro-link').on('click', function(e){
+		e.preventDefault();
+		showSection(intro, analysis);
+	
+	});
+
+
+	$('.analysis-link').on('click', function(e){
+		e.preventDefault();
+		showSection(analysis, intro);
+	
+		
+	});
+
 
 });
 
